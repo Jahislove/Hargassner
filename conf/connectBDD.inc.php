8a003f6aec8cedd3ad652cece16f93f7d81cff0c
Jahislove <tri-vertical@wanadoo.fr>
2015-12-26 13:15:26 +0100

    connectBDD initial
    without password

---
+++ conf/connectBDD.inc.php
@@ -0,0 +1,15 @@
+﻿<?php
+// MySQL config : a remplacer par vos parametres
+    $hostname = "localhost";
+    $database = "Hargassner";
+    $username = "hargassner";
+    $password = "**********";
+
+	
+// *****************************do not modify below***************************************
+// ************* connection to MySQL, **********************************************
+    function connectMaBase($hostname, $database, $username, $password){
+		$Conn = mysql_connect ($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);  
+		mysql_select_db($database, $Conn);
+    }
+?>
