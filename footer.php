e1e5d49610f19e3bb3e1f05ea90eede2b16444b4
Jahislove <tri-vertical@wanadoo.fr>
2016-03-20 12:58:27 +0100

    ajout numero version footer

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <footer>
+<?php
+    require_once("conf/version.php");
+?>
     Powered by  <a href="http://www.hargassner.fr"><IMG SRC="img/logo.png" ALT="logo" WIDTH=50 HEIGHT=26 ></a></br> 
-	Copyright JaHisLove 
+	Copyright JaHisLove , version : <?php echo $version ;?>
 </footer>
 </body>
 </html>
