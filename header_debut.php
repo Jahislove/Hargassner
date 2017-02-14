befda6126d0f1d38749e1b542ea13007c873bd00
Jahislove <jahislove@wanadoo.fr>
2017-02-14 23:29:39 +0100

    correctif 1.8

--- header_debut.php
+++ header_debut.php
@@ -20,12 +20,12 @@
     <script src="js/datepicker/bootstrap-datepicker.min.js"></script><!-- utilisé uniquement pour le datepicker -->
     <script src="js/datepicker/bootstrap-datepicker.fr.min.js" charset="UTF-8"></script><!-- utilisé uniquement pour le datepicker -->
     
-	<script src="js/highcharts.js"></script> 
+	<!-- <script src="js/highcharts.js"></script> --> 
 	<!-- <script src="js/highcharts-more.js"></script> --> 
-	<script src="js/exporting.js"></script> 
-	<!-- <script src="https://code.highcharts.com/highcharts.js"></script> -->
+	 
+	<script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/highcharts-more.js"></script>
-    
+    <script src="js/exporting.js"></script>
 
 <script type="text/javascript">	
     var histo_live_shift = <?php echo $histo_live_shift;?>;
