8f3e1f2441e17f56f26ac33c643ff8a43945000e
Jahislove <jahislove@wanadoo.fr>
2018-01-08 22:31:04 +0100

    injection manuelle des temperature

--- injection.php
+++ injection.php
@@ -1,17 +1,11 @@
 <?php
+// cette page permet d'injecter manuellement les temperatures interieures dans la BDD
+// format du fichier csv :
+//utc,local,celsius
+//2017-10-03T14:36:15+00:00,2017-10-18T01:30:15+01:00,20.2
+//2018-01-03T14:36:22+00:00,2017-10-18T01:31:22+01:00,20.2
+
 require_once("conf/config.inc.php");
-
-
-$hostname = "127.0.0.1"; //127.0.0.1 si la BDD est sur la meme machine que le serveur web , sinon IP
-$portSQL  = "3306";
-$database = "test"; // nom de la BDD
-$username = "hargassner"; // utilisateur mysql
-$password = "password";
-
-$conn = mysqli_connect ($hostname, $username, $password, $database, $portSQL); 
-if (!$conn) {
-	die("Connection BDD failed: " . mysqli_connect_error());
-}
 
 //variable provenant de php
 $fichier_ori = $_FILES['data']['name'] ; // nom du fichier d'origine
@@ -35,6 +29,8 @@ if ( rtrim($data[0]) != 'utc,local,celsius') { // verification format du csv
 }
 
 // traitement 
+connectMaBase($hostname, $database, $username, $password);
+
 for ($i = 1; $i < count($data); $i++){ // pour chaque ligne du tableau
 	list($UTC, $GMT, $temp) = explode(",",$data[$i]);//separe les champs
 
@@ -42,30 +38,21 @@ for ($i = 1; $i < count($data); $i++){ // pour chaque ligne du tableau
 	$GMT = substr($GMT, 0, -9); // supprime les 9 caracteres de la fin( secondes et l'heure gmt)
 	$temp = round($temp,1); // arrondi la temperature a 1 chiffre apres la virgule
 
-	$requete = "UPDATE data SET c138 = '$temp'
-				WHERE dateB LIKE '$GMT:__'";
-
+	//recherche l'id correspondant  a la date
+	$reqid = "select id from data WHERE dateB LIKE '$GMT:__'";
+	$result = mysql_query($reqid);
+    $id = mysql_fetch_row($result);
+	
+	// utilise l'id pour updater
+	$requete = "UPDATE data SET c138 = '$temp' WHERE id = $id[0]";
+				
 	echo "<br>" . $requete;
 	//injection dans la BDD
-	if (mysqli_query($conn, $requete)) {
+	if (mysql_query($requete)) {
 		echo " => record updated successfully";
 	} else {
-		echo " => Error: " . mysqli_error($conn);
+		echo " => Erreur " ;
 	}
-
-
 }
-
-	
-	
-	
-
-	
-	
-	
-
-
-	mysqli_close($conn);
-
-
+mysql_close();
 ?>
