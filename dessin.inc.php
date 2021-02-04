c2519e2f7dc77a2b1dbd84b13e4f3b400484ff2d
Jahislove <jahislove@wanadoo.fr>
2021-02-04 16:55:27 +0100

    tooltip ok

--- dessin.inc.php
+++ dessin.inc.php
@@ -14,76 +14,61 @@
 	</div>
     
 	<div id='bulles' class="visible" onclick="clic()">  
-	<!-- <span class='tooltipContainer'> -->
 		<div class='tooltipContainer extr-bulle '>
-			<span class="tooltiptext">Puissance du ventilateur d'extraction des fumées</span>
+			<span class="tooltipBulle">Puissance du ventilateur d'extraction des fumées</span>
 			<span id="extr-texte"  class='texte-bulle'></span>
 		</div>
-	<!-- </span> -->
 		<span class='tooltipContainer fumee-bulle'>
-			<span class="tooltiptext">Température des fumées</span>
+			<span class="tooltipBulle">Température des fumées</span>
 			<span id="fumee-texte" class='texte-bulle'></span>
 		</span>
 	
 		<span class='tooltipContainer Tchaud-bulle'>
-			<span class="tooltiptext">Température de l'eau</span>
+			<span class="tooltipBulle">Température de l'eau</span>
 			<span id="Tchaud-texte" class='texte-bulle'></span>
 		</span>
 	
 		<span class='tooltipContainer puiss-bulle'>
-			<span class="tooltiptext">Puissance</span>
+			<span class="tooltipBulle">Puissance</span>
 			<span id="puiss-texte"  class='texte-bulle'></span>
 		</span>
         
         <div class='maison-bulle'>
-			<!-- <div class='tooltipContainer tooltip_Tint'> -->
-				<!-- <span id="Tint-texte"   class='maison-Tint-texte'></span> -->
-				<!-- <span class="tooltiptext">Température intérieure</span> -->
-			<!-- </div> -->
-			<div class='tooltip_Maison maison-Tint-texte'>
+			<div class='tooltipContainer maison-Tint-texte'>
 				<div id="Tint-texte" ></div>	
-				<span class="tooltipText">Température intérieure</span>
+				<span class="tooltipMaison">Température intérieure</span>
 			</div>
-			<div class='tooltip_Maison maison-Text-texte'>
+			<div class='tooltipContainer maison-Text-texte'>
 				<span id="Text-texte"></span>
-				<span class="tooltipText">Température extérieure</span>
+				<span class="tooltipMaison">Température extérieure</span>
 			</div>
-			<div class='tooltip_Maison radiateur-texte'>
+			<div class='tooltipContainer radiateur-texte'>
 				<span id="radiateur-texte"></span>
-				<span class="tooltipText">température de départ chauffage</span>
+				<span class="tooltipMaison">température de départ chauffage</span>
 			</div>
 
-			<span id="pompe-radiat" class='tooltip_Maison pompeOFF'>
-				<span class="tooltiptext">pompe radiateur</span>
+			<span id="pompe-radiat" class='tooltipContainer pompeOFF'>
+				<span class="tooltipBulle">pompe radiateur</span>
 			</span>
 			<div id="ballonECS-bulle" class='hidden'>
 				<span id="tuyau-ECS" class='tuyau-ECS'></span>				
 				<span id="ballonECS-texte"   class='BallonECS-texte'></span>
-				<div id="pompe-ECS" class='tooltip_Maison pompeOFF'>
-					<span class="tooltiptext">pompe ECS</span>
+				<div id="pompe-ECS" class='tooltipContainer pompeOFF'>
+					<span class="tooltipBulle">pompe ECS</span>
 				</div>
 			</div>
-
-			<!-- <div id="ballonECS-bulle" class='hidden'> -->
-				<!-- <span class="tooltiptext">Ballon ECS</span> -->
-				<!-- <span id="ballonECS-texte"   class='BallonECS-texte'></span> -->
-				<!-- <div id="pompe-ECS" class='pompeOFF tooltipContainer'> -->
-					<!-- <span class="tooltiptext">pompe ECS</span> -->
-				<!-- </div> -->
-				<!-- <span id="tuyau-ECS" class='tuyau-ECS'></span>				 -->
-			<!-- </div> -->
         </div>
 
 		<span id="silo-bulle" class='silo-bulle'>
 			<span class='tooltipContainer'>
-				<span class="tooltiptext">quantité de granulés restant</span>
+				<span class="tooltipBulle">quantité de granulés restant</span>
 				<span id="silo-texte"   class='silo-texte'></span>
 				<img id="silo" src="img/Silo-textile-GWTS-XXL2.png"></img>
 			</span>
 		</span>
 
 		<span class='tooltipContainer bois-bulle'>
-			<span class="tooltiptext">pourcentage d'amené de la vis à granulé</span>
+			<span class="tooltipBulle">pourcentage d'amené de la vis à granulé</span>
 			<span id="bois-texte"   class='texte-bulle'></span>
 		</span>
 	</div>
